8665589160d3398ffce29161d4260ab5779587cb
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-01-05 20:14:51 -0500

    update readme

---
+++ spec/unit/permission_utilities_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe Authorizable::PermissionUtilities do
+
+  describe 'permissions' do
+
+  end
+
+  describe 'set_for_role' do
+
+  end
+
+  describe 'has_procs?' do
+
+  end
+
+  describe 'has_visibility_procs?' do
+
+  end
+
+  describe 'should_render?' do
+
+  end
+
+  describe 'name_for' do
+
+  end
+
+  describe 'value_for' do
+
+  end
+
+  describe 'has_key?' do
+
+  end
+
+  describe 'is_access?' do
+
+  end
+
+  describe 'is_object?' do
+
+  end
+end
