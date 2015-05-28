77cfe3f15fc08f8a7449c03c949e134c7dc5c1a8
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-05-28 16:05:54 -0400

    work on defaults - I'm thinking I don't need to actually specify them

--- lib/authorizable/controller/default_config.rb
+++ lib/authorizable/controller/default_config.rb
@@ -3,17 +3,30 @@ module Authorizable
     module DefaultConfig
 
       index_config = {
-
+        permission: :view_all
+        redirect_path: Proc.new{ root_path }
       }
+
+      create_config = {
+        redirect_path: Proc.new{ action: :index }
+      }
+
+      edit_config = {
+        redirect_path: Proc.new{ action: :index }
+      }
+
+      update_config = edit_config
+
+      destroy_config = {}
 
       def self.config
         {
-          index: {},
-          create: {},
-          show: {},
-          edit: {},
-          update: {},
-          destroy: {}
+          index: index_config,
+          create: create_config,
+          show: show_config,
+          edit: edit_config,
+          update: update_config,
+          destroy: destroy_config
         }
       end
 
